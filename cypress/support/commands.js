9585527dbbe558f34e4a8f5b1e17863bc51fda50
EDusik <eduardodusik@gmail.com>
2021-04-25 19:03:59 -0300

    feat(test): config Cypress

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,10 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+/* eslint-disable no-undef */
+import { environment } from "../../src/environments/environments";
+
+Cypress.Commands.add("goToTheHomePage", () => {
+	cy.visit("/");
+});
+
+Cypress.Commands.add("getTransactions", () => {
+	cy.route("GET", `${environment.baseUrl}transactions`, "fixture:transactions.json");
+});
