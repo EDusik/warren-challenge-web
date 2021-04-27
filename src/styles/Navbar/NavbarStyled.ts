eb5396f91ba8a41488c77ce044f42fd578c1562a
EDusik <eduardodusik@gmail.com>
2021-04-27 11:57:24 -0300

    style: adding Navbar styles

--- src/styles/Navbar/NavbarStyled.ts
+++ src/styles/Navbar/NavbarStyled.ts
@@ -5,6 +5,9 @@ export const NavbarStyled = styled.nav`
   font-size: 2rem;
   font-weight: 500;
   padding: 1rem;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
   
   span {
     font-weight: 600;
